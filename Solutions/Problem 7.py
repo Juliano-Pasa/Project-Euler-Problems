7909753054cc52f1fa63fe9726b8a5eb3a808d94
Juliano-Pasa <julianodemellopasa@gmail.com>
2021-10-14 18:36:01 -0300

    Problem 7

---
+++ Solutions/Problem 7.py
@@ -0,0 +1,27 @@
+from math import ceil, log
+
+def nth_prime_upper_bound(n):
+    if n < 6:
+        return 100
+    return ceil(n * (log(n) + log(log(n))))
+
+
+def find_nth_prime(index):
+    limit = nth_prime_upper_bound(index)
+
+    primes = [True] * limit
+    primes[0] = primes[1] = False
+    total = 0
+
+    for p, val in enumerate(primes):
+        if val:
+            primes[p*2::p] = [False] * len(primes[p*2::p])
+            total += 1 # Keeps track of the number of primes
+
+        if total == index:
+            return p
+
+
+if __name__ == "__main__":
+    ans = find_nth_prime(10_001)
+    print(ans)
