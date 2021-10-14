97653b986e380fec7f3d9fb96472daa66b065f8a
Juliano-Pasa <julianodemellopasa@gmail.com>
2021-10-14 11:18:01 -0300

    Problem 1

--- Solutions/Problem 1.py
+++ Solutions/Problem 1.py
@@ -1,12 +1,12 @@
-def sum_multiples(a, b, lim): # First solution
-    sum = 0
-    for i in range(lim): 
-        sum += i * (i % a == 0 or i % b == 0)
-    return sum
+def sum_multiples(a, b, limit): # First solution
+    ans = 0
+    for i in range(limit): 
+        ans += i * (i % a == 0 or i % b == 0)
+    return ans
 
-def sum_multiples_list(a, b, lim): # Solution found online
-    return sum(i for i in range(lim) if (i % a == 0 or i % b == 0))
+def sum_multiples_list(a, b, limit): # Solution found online
+    return sum(i for i in range(limit) if (i % a == 0 or i % b == 0))
 
 if __name__ == "__main__":
-    total = sum_multiples_list(3, 5, 1000)
-    print(total)
+    ans = sum_multiples_list(3, 5, 1000)
+    print(ans)
