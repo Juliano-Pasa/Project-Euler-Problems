157a4f0f16ec2793331916f448410bacb86f9178
Juliano-Pasa <julianodemellopasa@gmail.com>
2021-10-14 18:35:53 -0300

    Problem 6

---
+++ Solutions/Problem 6.py
@@ -0,0 +1,13 @@
+def calc_diff(limit):
+    sum_of_squares = 0
+    square_of_sum = 0
+
+    for i in range(1, limit):
+        sum_of_squares += i**2
+        square_of_sum += i
+    
+    return (square_of_sum**2) - sum_of_squares
+
+if __name__ == "__main__":
+    ans = calc_diff(101)
+    print(ans)
