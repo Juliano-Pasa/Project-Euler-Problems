8cc191dac51a7a0db979fd1628f823a49a10b582
Juliano-Pasa <julianodemellopasa@gmail.com>
2021-10-18 12:08:05 -0300

    Problem 10

---
+++ Solutions/Problem 10.py
@@ -0,0 +1,15 @@
+def find_primes_sum(limit): 
+    primes = [True] * limit # Sieve of eratosthenes
+    primes[0] = primes[1] = False
+    total = 0
+
+    for p, val in enumerate(primes):
+        if val: # All primes will be True
+            primes[p*2::p] = [False] * len(primes[p*2::p]) # All prime multiples become False
+            total += p # Sums all primes
+    
+    return total
+
+if __name__ == "__main__":
+    ans = find_primes_sum(2_000_000)
+    print(ans)
