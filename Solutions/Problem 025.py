2193236e198a24b8daaec22c984552525bb8d023
Juliano Pasa <julianodemellopasa@gmail.com>
2023-09-13 16:26:27 -0300

    025 solved

---
+++ Solutions/Problem 025.py
@@ -0,0 +1,20 @@
+from logging.handlers import NTEventLogHandler
+import math
+
+calculatedFibos = {}
+
+def nDigitFiboIndex(n):
+    a = 1
+    b = 1
+    c = 2
+    index = 2
+
+    while (math.floor(math.log10(c)) + 1) < n:
+        c = a + b
+        a = b
+        b = c
+        index += 1
+    return index
+
+if __name__ == "__main__":
+    print(nDigitFiboIndex(1000))
